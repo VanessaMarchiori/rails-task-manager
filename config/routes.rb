fd222350ccb20d9fabbad30b09f643bedd8d4e53
vanessa.x.marchiori@gmail.com <vanessa.x.marchiori@gmail.com>
2023-11-21 11:41:33 +0100

    first files

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+As a user, I can list tasks
+As a user, I can view the details of a task
+As a user, I can add a new task
+As a user, I can edit a task (mark as completed / update title & details)
+As a user, I can remove a task
