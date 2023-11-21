b260177a6a44755dc3116f93b4e33badfcf4d676
vanessa.x.marchiori@gmail.com <vanessa.x.marchiori@gmail.com>
2023-11-21 16:07:26 +0100

    tasks controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,19 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'tasks#index'
+
+  get "tasks/new", to:"tasks#new"
+
+  post "tasks", to:"tasks#create"
+  delete "tasks/:id", to: "tasks#destroy"
+
+  get "tasks/:id/edit", to:"tasks#edit", as: "edit_task"
+  patch "tasks/:id", to:"tasks#update"
+
+  get 'tasks/:id', to: 'tasks#show', as: "task"
+  # Defines the root path route ('/')
+  # root 'posts#index'
 end
-As a user, I can list tasks
-As a user, I can view the details of a task
-As a user, I can add a new task
-As a user, I can edit a task (mark as completed / update title & details)
-As a user, I can remove a task
